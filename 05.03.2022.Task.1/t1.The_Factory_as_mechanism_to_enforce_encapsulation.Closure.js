d938b4dea85ad542a318a405e5a79a3d7aab63ff
pa4080 <spas.z.spasov@gmail.com>
2022-03-06 00:36:14 +0200

    Object Private Members
    https://wwwcourses.github.io/ProgressBG-JS-Advanced-React-Slides/pages/themes/OOPinJS/OOPinJS.html#/taskTheFactoryAsMechanismToEnforceEncapsulation

---
+++ 05.03.2022.Task.1/t1.The_Factory_as_mechanism_to_enforce_encapsulation.Closure.js
@@ -0,0 +1,41 @@
+"use strict";
+const devFactory = function(name, salary) {
+    const _sensitive = {
+        salary: salary,
+        pwd: 'abracadabra'
+    }
+    
+    return {
+        name: name,
+        getSalary: function() {
+            return _sensitive.salary;
+        },
+        increaseSalary: function(value, pwd) {
+            if (pwd === _sensitive.pwd)
+                _sensitive.salary += value;
+            else
+                console.log(`Wrong password! ${this.name} salary will not be increased!`);
+        }
+    }
+}
+
+const dev1 = devFactory('Peter', 1000);
+const dev2 = devFactory('Maria', 1200);
+
+console.log(`${dev1.name} salary is ${dev1.getSalary()}`);
+console.log(`${dev2.name} salary is ${dev2.getSalary()}`);
+
+dev1.increaseSalary(500, 'abracadabra');
+dev2.increaseSalary(100, '123');
+
+console.log(`${dev1.name} salary after promotion is ${dev1.getSalary()}`);
+console.log(`${dev2.name} salary after promotion is ${dev2.getSalary()}`);
+
+// *** OUTPUT ***
+// Peter salary is 1000
+// Maria salary is 1200
+
+// Wrong password! Maria salary will not be increased!
+
+// Peter salary after promotion is 1500
+// Maria salary after promotion is 1200
---
+++ 05.03.2022.Task.1/t1.The_Factory_as_mechanism_to_enforce_encapsulation.WeakMap.js
@@ -0,0 +1,51 @@
+"use strict";
+const _sensitive = new WeakMap();
+
+const devFactory = function(name, salary) {
+    const obj = {
+        name: name,
+        getSalary: function() {
+            return _sensitive.get(this).salary;
+        },
+        increaseSalary: function(value, pwd) {
+            if (pwd === _sensitive.get(this).pwd)
+                _sensitive.get(this).salary += value;
+            else
+                console.log(`Wrong password! ${this.name} salary will not be increased!`);
+        }
+    };
+
+    _sensitive.set(obj, {
+        salary: salary,
+        pwd: 'abracadabra'
+    });
+    
+    return obj;
+}
+
+const dev1 = devFactory('Peter', 1000);
+const dev2 = devFactory('Maria', 1200);
+
+console.log(`${dev1.name} salary is ${dev1.getSalary()}`);
+console.log(`${dev2.name} salary is ${dev2.getSalary()}`);
+
+dev1.increaseSalary(500, 'abracadabra');
+dev2.increaseSalary(100, '123');
+
+console.log(`${dev1.name} salary after promotion is ${dev1.getSalary()}`);
+console.log(`${dev2.name} salary after promotion is ${dev2.getSalary()}`);
+
+// *** OUTPUT ***
+// Peter salary is 1000
+// Maria salary is 1200
+
+// Wrong password! Maria salary will not be increased!
+
+// Peter salary after promotion is 1500
+// Maria salary after promotion is 1200
+
+console.log(dev1.name, _sensitive.get(dev1));
+console.log(dev2.name, _sensitive.get(dev2));
+
+// Peter {salary: 1500, pwd: 'abracadabra'}
+// Maria {salary: 1200, pwd: 'abracadabra'}
