6a60b6bbc0bf72d42dc68a1e9446a35b17df5eeb
pa4080 <spas.z.spasov@gmail.com>
2022-03-07 19:58:55 +0200

    MDN README.md Print Prepare

--- MDN.Exercises/prepare.readme.md.js
+++ MDN.Exercises/prepare.readme.md.js
@@ -1,4 +1,6 @@
-// Run this function in the browser's console, before you print the README.md page.
+/**
+ * Run this function in the browser's console, before you print the README.md page.
+ */
 (function() {
     const elementsToRemove = document.body.querySelectorAll('#repository-container-header,.file-navigation,.f4.mt-3,.Box.mb-3,.d-flex.js-sticky.top-0');
     elementsToRemove.forEach((node) => {node.parentNode.removeChild(node);});
