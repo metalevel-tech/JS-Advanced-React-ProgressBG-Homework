13cd6146b5fac78ee49279b066d3cd9f5de03c7f
pa4080 <spas.z.spasov@gmail.com>
2022-03-07 19:58:03 +0200

    Labs tidy up

--- MDN.Exercises/prepare.readme.md.js
+++ MDN.Exercises/prepare.readme.md.js
@@ -1,3 +1,4 @@
+// Run this function in the browser's console, before you print the README.md page.
 (function() {
     const elementsToRemove = document.body.querySelectorAll('#repository-container-header,.file-navigation,.f4.mt-3,.Box.mb-3,.d-flex.js-sticky.top-0');
     elementsToRemove.forEach((node) => {node.parentNode.removeChild(node);});
