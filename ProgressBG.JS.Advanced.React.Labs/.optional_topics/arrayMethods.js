3b22a9df8fbee0a10393daf829283fd1486e993e
pa4080 <spas.z.spasov@gmail.com>
2022-03-24 13:51:50 +0200

    homework of lesson 7 - 20.03.2022

--- ProgressBG.JS.Advanced.React.Labs/.optional_topics/arrayMethods.js
+++
@@ -1,4 +0,0 @@
-// Array.from(arrayLike [, mapFn [, thisArg]])
-let arr = Array.from({length:10}, (_,i)=>i+1);
-console.dir(arr);
-
--- ProgressBG.JS.Advanced.React.Labs/lab1/template_strings.js
+++
@@ -1,7 +0,0 @@
-let userName = 'Ada';
-
-console.log('Hello,' + userName + '!');
-console.log(`Hello,${userName}!`);
-
-
-// 'Hello,Ada!'
