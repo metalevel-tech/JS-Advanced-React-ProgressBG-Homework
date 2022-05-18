890dc2fa1cb1852cbcfa59f57dc369ea35b6a84c
pa4080 <spas.z.spasov@gmail.com>
2022-05-18 22:05:14 +0300

    typescrypt basics recap

--- homework/hw.2022.05.08.typescript.basics/dist/example-4.types.js
+++ homework/hw.2022.05.08.typescript.basics/dist/example-4.types.js
@@ -20,7 +20,28 @@ var gColor = Color.Green;
 var anything = 42;
 anything = "hello";
 anything = true;
+// void is a little like the opposite of any:
+// the absence of having any type at all. You may 
+// commonly see this as the return type of functions
+// that do not return a value:
 function showMessage(message) {
     console.log(message);
 }
 showMessage("Hello World");
+showMessage(anything);
+var questionable = 4;
+showMessage(questionable);
+questionable = "hello";
+showMessage(questionable);
+questionable = true;
+showMessage(questionable);
+// Unlike unknown, variables of type any allow you to access arbitrary properties,
+// even ones that don’t exist. These properties include functions and TypeScript
+// will not check their existence or type:
+var looselyTyped = 4;
+// OK, ifItExists might exist at runtime
+looselyTyped.ifItExists();
+// OK, toFixed exists (but the compiler doesn't check)
+looselyTyped.toFixed();
+var strictlyTyped = 4;
+// strictlyTyped.toFixed(); // Object is of type 'unknown'.
